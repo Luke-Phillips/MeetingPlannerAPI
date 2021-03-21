1e4c03addcbc0ae05aab04b3bb10909ac502c30c
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-03-20 20:51:00 -0600

    Model added

---
+++ Models/MeetingProgram.cs
@@ -0,0 +1,21 @@
+﻿//using MongoDB.Bson;
+//using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace MeetingPlannerAPI.Models
+{
+    public class MeetingProgram
+    {
+        public string Id { get; set; }
+
+        public string Date { get; set; }
+        public string Conductor { get; set; }
+        public string OpeningSong { get; set; }
+        public string SacramentHymn { get; set; }
+        public string SpecialSong { get; set; }
+        public string ClosingSong { get; set; }
+        public string OpeningPrayer { get; set; }
+        public string ClosingPrayer { get; set; }
+        public List<Speaker> Speakers { get; set; }
+    }
+}
---
+++ Models/Speaker.cs
@@ -0,0 +1,11 @@
+﻿//using MongoDB.Bson;
+//using MongoDB.Bson.Serialization.Attributes;
+
+namespace MeetingPlannerAPI.Models
+{
+    public class Speaker
+    {
+        public string Name { get; set; }
+        public string Topic { get; set; }
+    }
+}
